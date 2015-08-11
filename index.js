3067c97197df632cc7ff1c341a52f7c13144635e
michaelgu95 <michaelgu95@gmail.com>
2015-08-11 12:50:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+
+var osipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var osport = process.env.OPENSHIFT_NODEJS_PORT;
+app.set('port', osport || 3000);
+app.set('ipaddress', osipaddress);
+
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.OPENSHIFT_NODJS_PORT || 3000;
+
+
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+// users trying to play
+var quickPlayUsers = new Array();
+
+io.on('connection', function(socket) {
+  var addedUser = false;
+
+  //=== Create and Join sockets ===
+  socket.on('join', function(data) {
+    socket.join(data.email); // We are using room of socket io
+  });
+
+  socket.on('joinGame', function(data){
+
+    //TODO change game room to variable 
+    io.to('jsmith@gmail.com').emit('userJoined', {msg: 'User Joined Game', game:data.game, user:data.user});
+  })
+
+
+  //=== Quick play sockets ===
+  socket.on('findOpponent', function(data){
+    socket.join(data.email);
+    quickPlayUsers.push(data);
+
+    if(quickPlayUsers.length >1){
+      var index = quickPlayUsers.length -1;
+      while(index >= 0){
+         var opponentData = quickPlayUsers[index];
+         if(opponentData != data && opponentData.subject == data.subject){
+            io.to(data.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:opponentData.email, subject:data.subject});
+            io.to(opponentData.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:data.email, subject:data.subject});
+            var userIndex = quickPlayUsers.indexOf(data);
+            socket.leave(data.email);
+            quickPlayUsers.splice(userIndex,1);
+            quickPlayUsers.splice(index,1);
+            break;
+         }else{
+          index--;
+         }
+      }
+    }
+  });
+
+  socket.on('leaveRoom', function(data){
+    socket.leave(data.email);
+  })
+
+
+});
