b702c3856ddf460270349598ec1d318a9478b960
michaelgu95 <michaelgu95@gmail.com>
2015-08-18 15:52:11 -0400

    working multiplayer gameEnded mode

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.static(__dirname + '/public'));
 
 // users trying to play
 var quickPlayUsers = new Array();
+var finishedUsers = new Array();
 
 io.sockets.on('connection', function(socket) {
   var addedUser = false;
@@ -35,19 +36,23 @@ io.sockets.on('connection', function(socket) {
   socket.on('findOpponent', function(data){
     socket.join(data.email);
     quickPlayUsers.push(data);
+    console.log(quickPlayUsers);
 
     if(quickPlayUsers.length >1){
       var index = quickPlayUsers.length -1;
+      dance:
       while(index >= 0){
          var opponentData = quickPlayUsers[index];
-         if(opponentData != data && opponentData.subject == data.subject){
-            io.to(data.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:opponentData.email, subject:data.subject, opponent:opponentData.user});
-            io.to(opponentData.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:data.email, subject:data.subject, opponent:data.user});
+         if(opponentData !== data && opponentData.subject == data.subject){
+            socket.emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:opponentData.email, subject:data.subject, opponent:opponentData.user});
+            socket.broadcast.to(opponentData.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:data.email, subject:data.subject, opponent:data.user});
             var userIndex = quickPlayUsers.indexOf(data);
-            socket.leave(data.email);
+            // socket.leave(data.email);
+            // socket.leave(opponentData.email);
             quickPlayUsers.splice(userIndex,1);
             quickPlayUsers.splice(index,1);
-            break;
+            console.log(quickPlayUsers);
+            break dance;
          }else{
           index--;
          }
@@ -56,29 +61,39 @@ io.sockets.on('connection', function(socket) {
   });
 
   socket.on('quitMatch', function(data){
-    io.to(data.opponentEmail).emit('opponentQuit', {msg:'Your Opponent Forfeited the Match'});
+    socket.broadcast.to(data.opponentEmail).emit('opponentQuit', {msg:'Your Opponent Forfeited the Match'});
   })
 
   socket.on('finishedGame', function(data){
+    socket.join(userEmail);
     var opponentEmail = data.opponentEmail;
     var userEmail = data.userEmail;
-    var firstUserDoneData = data;
+    var opponent = data.opponent;
+    var user = data.user;
 
-    userEmail.finished = true;
-    if(opponentEmail.finished == true){
-      io.to(opponentEmail).emit('opponentFinished', {score: data.score, wrongQuestions: data.wrongQuestions, rightQuestions: data.rightQuestions});
-    }else{
-      socket.emit('waitingForOpponent', {msg: 'Waiting For Opponent To Finish'});
-      while(!opponentEmail.finished){
-        socket.once('finishedGame', function(data){
-          if(data.opponentEmail = userEmail){
-              io.to(opponentEmail).emit('opponentFinished', {score: firstUserDoneData.score, wrongQuestions: firstUserDoneData.wrongQuestions, rightQuestions: firstUserDoneData.rightQuestions});
-          }
-        })
-      }
-    }
+    //save the data to the user, containing right/wrong questions and user's score
     
+    finishedUsers[userEmail] = data; 
+    console.log(finishedUsers);
+    //check if opponent has finished by seeing if it exists in finishedUsers array
+    if(opponentEmail in finishedUsers){
+      //report back to user that opponent finished
+      var finishedOpponentData = finishedUsers[opponentEmail];
+      socket.emit('opponentStatus', {msg:'Finished', opponentData: finishedOpponentData});
+
+      //report to opponent that user is done
+      socket.broadcast.to(opponentEmail).emit('opponentFinished', {score: data.score, wrongQuestions: data.wrongQuestions, rightQuestions: data.rightQuestions});
+      socket.leave(userEmail);
+      socket.leave(data.email);
+    }else{
+      socket.emit('opponentStatus', {msg: 'Waiting'});
+      // while(!opponentEmail.finished){
+      //   socket.once('finishedGame', function(data){
+      //     if(data.opponentEmail = userEmail){  
+      //         io.to(opponentEmail).emit('opponentFinished', {score: firstUserDoneData.score, wrongQuestions: firstUserDoneData.wrongQuestions, rightQuestions: firstUserDoneData.rightQuestions});
+      //     }`
+      //   })
+      // }
+    }
   })
-
-
 });
