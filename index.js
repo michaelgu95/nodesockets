cf5ad0ea680c915b0fa66ce4edd2805bc84f0966
michaelgu95 <michaelgu95@gmail.com>
2015-08-20 15:27:47 -0400

    change broadcast useremail

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ io.sockets.on('connection', function(socket) {
       while(index >= 0){
          var opponentData = quickPlayUsers[index];
          if(opponentData !== data && opponentData.subject == data.subject){
-            socket.emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:opponentData.email, subject:data.subject, opponent:opponentData.user});
+            socket.broadcast.to(data.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:opponentData.email, subject:data.subject, opponent:opponentData.user});
             socket.broadcast.to(opponentData.email).emit('opponentFound', {msg: 'Opponent Found!', opponentEmail:data.email, subject:data.subject, opponent:data.user});
             var userIndex = quickPlayUsers.indexOf(data);
             
@@ -79,7 +79,7 @@ io.sockets.on('connection', function(socket) {
     if(opponentEmail in finishedUsers){
       //report back to user that opponent finished
       var finishedOpponentData = finishedUsers[opponentEmail];
-      socket.emit('opponentStatus', {msg:'Finished', opponentData: finishedOpponentData});
+      socket.broadcast.to(userEmail).emit('opponentStatus', {msg:'Finished', opponentData: finishedOpponentData});
 
       //report to opponent that user is done
       socket.broadcast.to(opponentEmail).emit('opponentFinished', {score: data.score, wrongQuestions: data.wrongQuestions, rightQuestions: data.rightQuestions});
